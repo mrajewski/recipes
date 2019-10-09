346408dedea65815862501c2e9314d1bae227bd5
mrajewski <rajewski.ma@gmail.com>
2019-10-09 18:07:16 +0200

    Updates

---
+++ precache-manifest.e9ea95f3c7b5ee75855b389f51f63607.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adcc12c54c319088d3998eba1b6dde09",
+    "url": "/recipes/index.html"
+  },
+  {
+    "revision": "6311a8507a5770d183dc",
+    "url": "/recipes/static/css/2.2f2f8889.chunk.css"
+  },
+  {
+    "revision": "d7e654ff8a9c21811da9",
+    "url": "/recipes/static/css/main.17bd7bf6.chunk.css"
+  },
+  {
+    "revision": "6311a8507a5770d183dc",
+    "url": "/recipes/static/js/2.63bb0bd0.chunk.js"
+  },
+  {
+    "revision": "d7e654ff8a9c21811da9",
+    "url": "/recipes/static/js/main.47c70dc2.chunk.js"
+  },
+  {
+    "revision": "c96e90947479d05374fe",
+    "url": "/recipes/static/js/runtime~main.19e64e71.js"
+  },
+  {
+    "revision": "a87eca9ea0f39475f2fc7613a3af312f",
+    "url": "/recipes/static/media/24px.a87eca9e.svg"
+  }
+]);
